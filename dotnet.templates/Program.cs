3fb2a61ee482fad0d195c1cc74a008b67c10450a
Charlie Codel <45616785+charliecodel@users.noreply.github.com>
2021-07-24 16:43:56 -0400

    first commit

---
+++ dotnet.templates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet.templates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
